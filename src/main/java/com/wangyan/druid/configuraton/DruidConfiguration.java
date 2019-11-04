d3fc4043c278012fe40b48a51ab94da9b608b8e9
wangyan <wangyan37274@sina.com>
2019-11-04 18:11:21 +0800

    修改配置类

--- src/main/java/com/wangyan/druid/configuraton/DruidConfiguration.java
+++ src/main/java/com/wangyan/druid/configuraton/DruidConfiguration.java
@@ -1,22 +1,6 @@
 package com.wangyan.druid.configuraton;
 
-import com.alibaba.druid.filter.Filter;
-import com.alibaba.druid.filter.stat.StatFilter;
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-import com.alibaba.druid.wall.WallFilter;
-import com.alibaba.druid.wall.WallTableStat;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @Auther: wangyan
@@ -25,17 +9,4 @@ import java.util.Map;
 @Configuration
 public class DruidConfiguration {
 
-    //servlet配置过滤拦截
-    @Bean
-    public FilterRegistrationBean<WebStatFilter> webStatFilter() {
-        FilterRegistrationBean<WebStatFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new WebStatFilter());
-        registrationBean.setUrlPatterns(Collections.singletonList("/*"));
-        registrationBean.setEnabled(true);
-        //设置初始化参数
-        Map<String, String> initMap = new HashMap<>(1);
-        initMap.put("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
-        registrationBean.setInitParameters(initMap);
-        return registrationBean;
-    }
 }
