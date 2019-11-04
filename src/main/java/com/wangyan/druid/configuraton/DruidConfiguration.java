5db89fc0615ba13c4cd283e029dd594900ae30b6
wangyan <wangyan37274@sina.com>
2019-11-04 18:10:40 +0800

    修改配置文件

--- src/main/java/com/wangyan/druid/configuraton/DruidConfiguration.java
+++ src/main/java/com/wangyan/druid/configuraton/DruidConfiguration.java
@@ -25,29 +25,6 @@ import java.util.Map;
 @Configuration
 public class DruidConfiguration {
 
-    @Bean(initMethod = "init",destroyMethod = "close")
-    public DruidDataSource druid() {
-        return new DruidDataSource();
-    }
-
-    //servlet配置内置监控
-    @Bean
-    public ServletRegistrationBean<StatViewServlet> statViewServlet() {
-        ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<>();
-        registrationBean.setServlet(new StatViewServlet());
-        registrationBean.setUrlMappings(Collections.singletonList("/druid/*"));
-        registrationBean.setEnabled(true);
-        //设置初始化参数
-        Map<String, String> initMap = new HashMap<>();
-        initMap.put("loginUsername", "admin");
-        initMap.put("loginPassword", "123456");
-        initMap.put("allow", "");
-        initMap.put("deny", "");
-        initMap.put("resetEnable", "false");// 禁用HTML页面上的“Reset All”功能
-        registrationBean.setInitParameters(initMap);
-        return registrationBean;
-    }
-
     //servlet配置过滤拦截
     @Bean
     public FilterRegistrationBean<WebStatFilter> webStatFilter() {
@@ -60,18 +37,5 @@ public class DruidConfiguration {
         initMap.put("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
         registrationBean.setInitParameters(initMap);
         return registrationBean;
-    }
-
-    @Bean
-    public Filter StatFilter(){
-        StatFilter filter = new StatFilter();
-        filter.setLogSlowSql(true);
-        filter.setSlowSqlMillis(1000);
-        return filter;
-}
-
-    @Bean
-    public Filter WallFilter(){
-        return new WallFilter();
     }
 }
